61ce2670be5e98da16ed56138856ebad1172abfe
ben.strube <benjamin.strube@ust.com>
2021-12-16 20:48:53 -0500

    updated controllers

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -7,8 +7,6 @@ import com.pluralsight.conferencedemo.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,10 +25,5 @@ public class SessionsController {
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id) {
         return sessionRepository.getOne(id);
-    }
-
-    @PostMapping
-    public Session create(@RequestBody final Session session) {
-        return sessionRepository.saveAndFlush(session);
     }
 }

cb3c8cbf8b50cad2b31038da0b091f4bbcafdf73
ben.strube <benjamin.strube@ust.com>
2021-12-16 21:16:59 -0500

    added CRUD to speaker controller

--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,10 +25,31 @@ public class SpeakersController {
     public List<Speaker> list() {
         return speakerRepository.findAll();
     }
+
+    //create
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
     
+    //read
     @GetMapping
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id) {
         return speakerRepository.getOne(id);
+    }
+
+    //update
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+
+    //delete
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
     }
 }
